70dadd2fbaa5fd0003928cd92e4fd18184b2ea53
a.kovalev <a.kovalev@voskhod.ru>
2020-08-27 06:55:32 +0300

    минорные правки

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from app import app, db
 from app.models import Employee
-import daemon
 
 
 @app.shell_context_processor
@@ -8,6 +7,5 @@ def make_shell_context():
     return {'db': db, 'Employee': Employee}
 
 
-with daemon.DaemonContext():
-    if __name__ == "__main__":
-        app.run(debug=False, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
